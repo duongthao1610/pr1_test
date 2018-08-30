154e0cd5166e1867766eef35eb136a45489ad2ac
duongthao1610 <duongthao.1610@gmail.com>
2018-08-30 23:29:13 +0700

    Update profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :exams, dependent: :destroy
   validates :name, length: {maximum: 40}
+  validates_integrity_of :avatar
+  validates_processing_of :avatar
 end
